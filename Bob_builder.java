f2b1a2d47e4d9791efc2ff14d1ef2c7244f38b7f
Greeshmakkgupta <52143219+Greeshmakkgupta@users.noreply.github.com>
2020-04-13 01:36:37 +0530

    Update Bob_builder.java

--- Bob_builder.java
+++ Bob_builder.java
@@ -1,3 +1,10 @@
+Problem statement:ou are given N unit squares (squares with side length 1 unit), and you are asked to make rectangles using these squares. You have to count the number of rotationally unique rectangles than you can make. What does rotationally unique mean? Well, two rectangles are rotationally unique if one can’t be rotated to become equivalent to the other one.
+
+Example – The 4×2 rectangle can be rotated 90 degrees clockwise to make it the exact same as the 2×4 rectangle and so these are not rotationally unique.
+
+
+
+Solution:
 import java.util.Scanner;
 import java.lang.Math;
 class Kanak
